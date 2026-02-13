b318531b15119a713040fa8bedcfd5dfaf6a61d2
Hodaka <2841006960@qq.com>
2026-02-13 14:39:31 +0800

    Initial commit

---
+++ src/main/java/com/shuzi/managementplatform/ManagementPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.shuzi.managementplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shuzi/managementplatform/ManagementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuzi.managementplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
